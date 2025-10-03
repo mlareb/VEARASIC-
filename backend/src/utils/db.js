5ed3856a74d05b3ed9cbd7bd5db449f58a86502d
mlareb <mlarebrayeen@gmail.com>
2025-10-03 12:31:43 +0530

    Configured Knex with Render Postgres

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,11 +1,9 @@
-const knex = require('knex')({
-  client: 'pg',
-  connection: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || 'postgres',
-    database: process.env.DB_NAME || 'vearasic'
-  },
-  pool: {min:0, max:10}
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }  // Needed for Render Postgres
 });
-module.exports = knex;
+
+module.exports = db;
